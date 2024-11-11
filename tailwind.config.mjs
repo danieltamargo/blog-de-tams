a02c4094d7dbca02f36b4733e80d217b0625d669
Daniel Tamargo <danieltamargosaiz@gmail.com>
2024-11-11 22:17:29 +0100

    Blog v0.4

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      width: {
+        '4xl': '56rem',
+        '7xl': '86rem',
+      },
       typography: {
         DEFAULT: {
           css: {
