01ff6bd7defd4b84d1d752ce3df8a0f5772b32e0
Daniel Tamargo <danieltamargosaiz@gmail.com>
2025-01-30 11:58:20 +0100

    Blog v0.19 upgrade Astro, configurar prefetch y temas markdown, mejorar post principios SOLID, eliminar copia del post

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,20 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
+  
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: {
+    defaultStrategy: 'viewport', // Debemos indicar data-astro-prefetch="tipo" en los enlaces a precargar
+    prefetchAll: 'true', // Precarga todas las páginas (habilitado en Astro por defecto con las ViewTransitions)
+  },
+
   integrations: [mdx(), tailwind()],
   markdown: {
     shikiConfig: {
-      theme: 'dracula',
+      themes: {
+        dark: 'github-dark',
+        light: 'github-light'
+      },
       wrap: true
     }
   },
